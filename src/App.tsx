c864ae77b8ecd014901fe4563dc464ddfedfb0d1
Murillo Ressineti <murillobr22@gmail.com>
2026-02-03 12:27:40 -0300

    feat: Building the Icon and Tag components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
 import Text from "./components/text";
+import Icon from "./components/icon";
+import ArrowRight from "./assets/icons/arrow-right.svg?react";
+import Tag from "./components/tag";
 
 export default function App() {
-  return(
-    <div className="flex flex-col gap-2">
-      <Text variant="hero-desktop" className="text-feedback-error">Olá, mundo!</Text>
-      <Text variant="hero-mobile" className="text-feedback-error">Olá, mundo!</Text>
-      <Text variant="body-extra-large" className="text-feedback-success">Olá, mundo!</Text>
-      <Text variant="body-large" className="text-feedback-success">Olá, mundo!</Text>
-      <Text variant="body-base" className="text-feedback-success">Olá, mundo!</Text>
-      <Text variant="body-small" className="text-feedback-success">Olá, mundo!</Text>
+  return (
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-2">
+        <Text variant="hero-desktop" className="text-feedback-error">
+          Olá, mundo!
+        </Text>
+        <Text variant="hero-mobile" className="text-feedback-error">
+          Olá, mundo!
+        </Text>
+        <Text variant="body-extra-large" className="text-feedback-success">
+          Olá, mundo!
+        </Text>
+        <Text variant="body-large" className="text-feedback-success">
+          Olá, mundo!
+        </Text>
+        <Text variant="body-base" className="text-feedback-success">
+          Olá, mundo!
+        </Text>
+        <Text variant="body-small" className="text-feedback-success">
+          Olá, mundo!
+        </Text>
+        <Text variant="h1" className="text-brand-primary">
+          Olá, mundo!
+        </Text>
+        <Text variant="h2" className="text-brand-primary">
+          Olá, mundo!
+        </Text>
+        <Text variant="h3" className="text-brand-primary">
+          Olá, mundo!
+        </Text>
+        <Text variant="h4" className="text-brand-primary">
+          Olá, mundo!
+        </Text>
+        <Text variant="h5" className="text-brand-primary">
+          Olá, mundo!
+        </Text>
+        <Text variant="action-link" className="text-text-link">
+          Link
+        </Text>
+        <Text variant="action-button" className="text-text-link">
+          Link
+        </Text>
+        <Text variant="headline-medium" className="text-text-link">
+          Link
+        </Text>
+        <Text variant="headline-small" className="text-text-link">
+          Link
+        </Text>
+      </div>
 
-      <Text variant="h1" className="text-brand-primary">Olá, mundo!</Text>
-      <Text variant="h2" className="text-brand-primary">Olá, mundo!</Text>
-      <Text variant="h3" className="text-brand-primary">Olá, mundo!</Text>
-      <Text variant="h4" className="text-brand-primary">Olá, mundo!</Text>
-      <Text variant="h5" className="text-brand-primary">Olá, mundo!</Text>
+      <div className="flex gap-1">
+        <Icon svg={ArrowRight} className="fill-brand-primary" animate />
+      </div>
 
-      <Text variant="action-link" className="text-text-link">Link</Text>
-      <Text variant="action-button" className="text-text-link">Link</Text>
-      <Text variant="headline-medium" className="text-text-link">Link</Text>
-      <Text variant="headline-small" className="text-text-link">Link</Text>
+      <div>
+        <Tag dot="react">React</Tag>
+        <Tag dot="tailwind">Tailwind CSS</Tag>
+        <Tag dot="typescript">TypeScript</Tag>
+        <Tag dot="figma">Figma</Tag>
+        <Tag dot="git">Git</Tag>
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/components/tag.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "../lib/utils";
+import Text from "./text";
+
+export const tagVariants = cva(
+  "inline-flex items-center gap-4 px-3 py-1.5 rounded-lg border border-border-default bg-bg-default",
+  {
+    variants: {
+      variant: {
+        default: "",
+        outline: "bg-transparent",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+);
+
+const dotHexColors = {
+  react: "#61DAFB",
+  tailwind: "#38BDF8",
+  typescript: "#3178C6",
+  figma: "#874FFF",
+  git: "#F34F29",
+};
+
+interface TagProps
+  extends React.ComponentProps<"div">, VariantProps<typeof tagVariants> {
+  dot?: keyof typeof dotHexColors;
+  children: React.ReactNode;
+}
+
+export default function Tag({
+  variant,
+  dot = "react",
+  className,
+  children,
+  ...props
+}: TagProps) {
+  const selectedHex = dotHexColors[dot];
+
+  return (
+    <div className={cn(tagVariants({ variant, className }))} {...props}>
+      <span
+        className="h-3 w-3 rounded-full shrink-0"
+        style={{
+          backgroundColor: selectedHex,
+          // O valor 33 em hexadecimal é igual a 51 em decimal, que representa exatamente 20% de 255.
+          boxShadow: `0 0 0 4px ${selectedHex}33`,
+        }}
+      />
+
+      <Text variant="body-extra-large">{children}</Text>
+    </div>
+  );
+}
