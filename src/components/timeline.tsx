97721982bde969f8aaf65381af0f34c62199d9ca
Murillo Ressineti <murillobr22@gmail.com>
2026-02-24 17:33:23 -0300

    refactor: Refactoring the timeline component because it doesn't need the CVA.

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,38 +1,28 @@
 import React from "react";
-import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "../lib/utils";
 
-export const timelineVariants = cva("relative w-full max-w-6xl mx-auto flex flex-col", {
-  variants: {
-    variant: {
-      default: "",
-    },
-  },
-  defaultVariants: {
-    variant: "default",
-  },
-});
-
-interface TimelineProps
-  extends React.ComponentProps<"div">, VariantProps<typeof timelineVariants> {
+interface TimelineProps extends React.ComponentProps<"div"> {
   children: React.ReactNode;
 }
 
 export default function Timeline({
-  variant,
   children,
   className,
   ...props
 }: TimelineProps) {
   return (
-    <div className={cn(timelineVariants({ variant, className }))} {...props}>
-      <div className="absolute left-1/2 -top-10 -bottom-10 w-0.5 bg-border-default -translate-x-1/2 z-0"></div>
+    <div
+      className={cn(
+        "relative w-full max-w-6xl mx-auto flex flex-col",
+        className,
+      )}
+      {...props}
+    >
+      {/* Linha Central Vertical */}
+      <div className="absolute left-1/2 -top-10 -bottom-10 w-0.5 bg-border-default -translate-x-1/2 z-0 hidden md:block" />
 
-      <div className="relative z-10 flex flex-col">
-        {React.Children.map(children, (child) => {
-          return child;
-        })}
-      </div>
+      {/* Container dos Itens */}
+      <div className="relative z-10 flex flex-col">{children}</div>
     </div>
   );
 }
